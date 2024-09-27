75d742136bb1f53300f3ce8d0f0209ef3a6a0c4a
ShashankBharide <74668513+ShashankBharide@users.noreply.github.com>
2024-09-26 23:29:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;  // Set the port to use
 
 app.get('/', (req, res) => {
-  res.send('Hello Azure, from Jashan!');
+  res.send('Hello Azure!');  // Response for the root endpoint
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);  // Log the server status
 });
